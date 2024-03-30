76c5d27aba6226dc77375a00ff53debc54aa15e9
Kashish Solanki <kashishsolanki229@gmail.com>
2024-03-30 22:16:52 +0530

    added card component

--- hackathon-practice/src/App.jsx
+++ hackathon-practice/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css'
 import axios from 'axios'
 import { useEffect } from 'react';
+import Card from './components/Card';
 
 function App() {
   const [tutors, setTutors] = useState([]);
@@ -18,15 +19,19 @@ function App() {
 
   return (
     <>
-      <h1>{tutors.length}</h1>
+     <h1>Teacher Details</h1>
+    <div className='card-container'>
+   
+    
       {tutors.map((tutor) => {
         return (
-          <div key={tutor.id}>
-            <h1>{tutor.name}</h1>
-            <h2>{tutor.subTaught}</h2>
+          <div key={tutor.id} >
+           
+            <Card name={tutor.name} subject={tutor.subTaught} />
           </div>
         )
       })}
+      </div>
     </>
   )
 }
