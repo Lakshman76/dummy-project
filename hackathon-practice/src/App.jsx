2f8c60ffdf64de4b082905e8be4390c77bcbfeef
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-03-31 15:22:55 +0530

    conflict resolve

--- hackathon-practice/src/App.jsx
+++ hackathon-practice/src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from 'react';
 import './App.css'
 import axios from 'axios'
 import { useEffect } from 'react';
-<<<<<<< HEAD
-import Register from './Components/Register';
-=======
 import Card from './components/Card';
 
->>>>>>> 76c5d27aba6226dc77375a00ff53debc54aa15e9
 function App() {
   const [tutors, setTutors] = useState([]);
 
@@ -23,15 +19,10 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <Register/>
-      <h1>{tutors.length}</h1>
-=======
      <h1>Teacher Details</h1>
     <div className='card-container'>
    
     
->>>>>>> 76c5d27aba6226dc77375a00ff53debc54aa15e9
       {tutors.map((tutor) => {
         return (
           <div key={tutor.id} >
@@ -40,11 +31,7 @@ function App() {
           </div>
         )
       })}
-<<<<<<< HEAD
-
-=======
       </div>
->>>>>>> 76c5d27aba6226dc77375a00ff53debc54aa15e9
     </>
   )
 }
